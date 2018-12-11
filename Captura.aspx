88d8e2b36caf71dca946432df077d912ee2df47a
LucasAMatos <45777124+LucasAMatos@users.noreply.github.com>
2018-12-11 14:20:04 -0200

    Add files via upload

---
+++ Captura.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="Captura de Arquivo" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Captura.aspx.cs" Inherits="CapturaArquivoWeb.Captura" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %></h2>
+    <div>
+        <table Width="450px">
+            <tr>
+                <td colspan="6">
+                     <span Width="253px">Resultado da captura</span>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="6">
+
+                    <asp:GridView ID="dgvResultado" runat="server" Width="450px" Height="250px" ShowHeader="False">
+                    </asp:GridView>
+
+                </td>
+            </tr>
+            <tr>
+                <td colspan="6">
+                    <asp:FileUpload ID="fulPesquisar" runat="server" Width="450px" OnLoad="fulPesquisar_Load" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnCarregar" runat="server" Text="Carregar" Width="80px" OnClick="btnCarregar_Click" />
+                </td>
+                <td>
+                    <asp:Button ID="btnGravar" runat="server" Text="Gravar" Width="80px" OnClick="btnGravar_Click" />
+                </td>
+                <td>
+                    <asp:Button ID="BtnSair" runat="server" Text="Sair" Width="80px" />
+                </td>
+            </tr>
+        </tabl>
+    </div>
+    </div>
+</asp:Content>
---
+++ Captura.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.UI;
+using System.IO;
+using System.Windows;
+using System.Web.UI.WebControls;
+
+namespace CapturaArquivoWeb
+{
+    public partial class Captura : Page
+    {
+        public string Tipo
+        {
+            get
+            {
+                return Request.Params["Tipo"].ToString();
+            }
+        }
+
+        protected string fileContent
+        {
+            get
+            {
+                return Session["fileContent"].ToString();
+            }
+            set
+            {
+                Session["fileContent"] = value;
+            }
+        }
+
+        protected DataSet dsCaptura
+        {
+            get
+            {
+                return (DataSet)Session["dsCaptura"];
+            }
+            set
+            {
+                Session["dsCaptura"] = value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Title = string.Concat("Captura de ", Tipo);
+        }
+
+        protected void fulPesquisar_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void btnCarregar_Click(object sender, EventArgs e)
+        {
+            //Read the contents of the file into a stream
+            var fileStream = fulPesquisar.FileContent;
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                fileContent = reader.ReadToEnd();
+            }
+
+            LeituraArquivo pLeituraArquivo = new LeituraArquivo();
+            if (!string.IsNullOrEmpty(fileContent))
+                dsCaptura = pLeituraArquivo.PreencheTabela(fileContent);
+            dgvResultado.DataSource = dsCaptura;
+            dgvResultado.DataBind();
+        }
+
+        protected void btnGravar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GravaArquivo grArq = new GravaArquivo();
+                grArq.GravarDados(Tipo, dsCaptura);
+                Response.Write("<script>alert('Captura Realizada com sucesso.');</script>");
+            }
+            catch (Exception E)
+            {
+                Response.Write("<script>alert('Houve Erro na captura. Favor validar com o suporte.');</script>");
+            }
+        }
+
+    }
+}
+
